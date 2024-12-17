5aab34b5656443d20d1ece8ba604df840153e6e0
Normankita <kitanorman1@gmail.com>
2024-12-17 08:32:30 +0300

    mobile number footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import '../assets/styles/Footer.scss'
 
 function Footer() {
@@ -10,6 +11,7 @@ function Footer() {
         <a href="https://github.com/Normankita" target="_blank" rel="noreferrer"><GitHubIcon/></a>
         <a href="https://www.linkedin.com/in/norman-kita-aa6499207/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
+      <a href="https://wa.me/+255621381584" target="_blank" rel="noreferrer"><WhatsAppIcon/></a>
       <p>A portfolio designed & built by <a href="https://github.com/Normankita/portifolio" target="_blank" rel="noreferrer">Norman Kita</a> with 💜</p>
     </footer>
   );
