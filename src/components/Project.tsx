7d9e6eddce9f515f2414933c1da1ad42bafbfa79
Normankita <kitanorman1@gmail.com>
2024-12-26 14:49:25 +0300

    fixed links

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -18,28 +18,28 @@ function Project() {
                 <p>Integrates the TMDB API for dynamic movie and TV show data fetching, including search and trending features. It uses a responsive design for a seamless user experience across devices. React efficiently manages state and renders content without reloading, enhancing interactivity and performance..</p>
             </div>
             <div className="project">
-                <a href="https://github.com/norman-kita/cybersecurity-tools" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/norman-kita/cybersecurity-tools" target="_blank" rel="noreferrer"><h2>NorZah CPRTask</h2></a>
+                <a href="https://norzah-todo.netlify.app/" target="_blank" rel="noreferrer"><img src={mock07} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://norzah-todo.netlify.app/" target="_blank" rel="noreferrer"><h2>NorZah CPRTask</h2></a>
                 <p>Web app to help you track your schedules and daily activities so you don't miss out any. utilizez local storage and state management utilities</p>
             </div>
             <div className="project">
-                <a href="https://github.com/norman-kita/data-visualization-dashboard" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/norman-kita/data-visualization-dashboard" target="_blank" rel="noreferrer"><h2>NorZah E-Commerce</h2></a>
+                <a href="https://norzah-ecommerce.netlify.app/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://norzah-ecommerce.netlify.app/" target="_blank" rel="noreferrer"><h2>NorZah E-Commerce</h2></a>
                 <p>Created a real-time E-commerce web application to demonstrate online purchases, powered by react and expressjs.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/norman-kita/data-visualization-dashboard" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/norman-kita/data-visualization-dashboard" target="_blank" rel="noreferrer"><h2>Word Counter</h2></a>
+                <a href="https://wordcount-norzah.netlify.app/" target="_blank" rel="noreferrer"><img src={mock01} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://wordcount-norzah.netlify.app/" target="_blank" rel="noreferrer"><h2>Word Counter</h2></a>
                 <p>Created a real-time copy and paste or write your own editor that helps you count words and characters within text</p>
             </div>
             <div className="project">
                 <a href="https://github.com/Normankita/TMH" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/norman-kita/reservation-system" target="_blank" rel="noreferrer"><h2>Reservation System</h2></a>
+                <a href="https://github.com/Normankita/TMH" target="_blank" rel="noreferrer"><h2>Reservation System</h2></a>
                 <p>Created a booking and reservation system for an apartment business using PHP, Yii Framework, and MySQL database integration.</p>
             </div>
             <div className="project">
-                <a href="https://github.com/norman-kita/ieee-802.1x-implementation" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
-                <a href="https://github.com/norman-kita/ieee-802.1x-implementation" target="_blank" rel="noreferrer"><h2>IEEE 802.1x Implementation</h2></a>
+                <a href="https://www.cisco.com/c/en/us/td/docs/ios-xml/ios/sec_usr_8021x/configuration/15-mt/sec-user-8021x-15-mt-book.pdf" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.cisco.com/c/en/us/td/docs/ios-xml/ios/sec_usr_8021x/configuration/15-mt/sec-user-8021x-15-mt-book.pdf" target="_blank" rel="noreferrer"><h2>IEEE 802.1x Implementation</h2></a>
                 <p>Developed and tested a secure port-based authentication protocol for VLANs using Cisco switches and Active Directory.</p>
             </div>
         </div>
